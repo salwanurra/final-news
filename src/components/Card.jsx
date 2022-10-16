01af57fcad298d5790a08e3cab53110d121685d8
Riandika <riandikasp@gmail.com>
2022-10-16 17:34:57 +0700

    Fix Card has different size when news title too short ( under 2 line )

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Card({ value }) {
   return (
-    <div className="p-4 rounded-md border border-2">
+    <div className="p-4 rounded-md border border-2 ">
       <img src={value.urlToImage || '/news.jpg'} alt="" className="rounded-md h-60 w-full object-cover" />
-      <h3 className="text-blue font-bold my-3 line-clamp-2">{value.title}</h3>
+      <h3 className="text-blue font-bold my-3 line-clamp-1">{value.title}</h3>
       <div className="h-28">
         <p className="text-light-grey line-clamp-4">{value.description || value.title}</p>
       </div>
