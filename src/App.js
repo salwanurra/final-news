82f0d1ef2daa0b3184cfb883605f8f41f79948e6
Riandika <riandikasp@gmail.com>
2022-10-20 06:08:19 +0700

    Scroll Back To Top onRefresh and transition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AnimatedRoutes from "./routes/AnimatedRoutes";
-import 'antd/dist/antd.css'
-
+import "antd/dist/antd.css";
+import ScrollToTop from "./routes/ScrollToTop";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  }, []);
   return (
     <div className="App">
       <div className="parent bg-white">
         <Router>
-          <div className="sticky top-0 bg-slate-800">
+          <ScrollToTop />
+
+          <div className="w-screen top-0 bg-slate-800 fixed z-10">
             <Navbar />
           </div>
-          <div className="justify-center flex items-center">
+          <div className="justify-center flex items-center mt-12">
             <AnimatedRoutes></AnimatedRoutes>
           </div>
         </Router>
